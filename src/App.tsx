d13211d80bfd9ef9556c90d28ddb7f2b8e5feae6
Juan Cabrera <juan@jjcp.dev>
2024-12-12 12:12:24 -0300

    A bit of fun

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,17 @@ export function App() {
   const [clip, isLoading] = useClip();
   return (
     <>
-      {isLoading && <h1>Loading...</h1>}
+      {isLoading && (
+        <>
+          <h1>Loading...</h1>
+          {/* Having a bit of fun :D */}
+          <img
+            style={{ width: "100vw", height: "100vh", objectFit: "contain" }}
+            src="//i.imgur.com/eL6jRH7.gif"
+          />
+        </>
+      )}
+
       {!isLoading && <Tool clip={clip} />}
     </>
   );
