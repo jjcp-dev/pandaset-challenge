98ab8fe2ca1bfc6131743e3ba8d691de68171cbc
Juan Cabrera <juan@jjcp.dev>
2024-12-12 01:04:35 -0300

    Add PointsCloud component

---
+++ src/PointsCloud.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useMemo, useRef } from "react";
+import { Color, Points, DynamicDrawUsage } from "three";
+
+type PointsCloudProps = {
+  maxPoints: number;
+  minZ: number;
+  maxZ: number;
+  pointSize: number;
+  points: number[][];
+  gradientFrom: Color;
+  gradientTo: Color;
+};
+
+export function PointsCloud({
+  maxPoints,
+  minZ,
+  maxZ,
+  points,
+  pointSize,
+  gradientFrom,
+  gradientTo,
+}: PointsCloudProps) {
+  const pointsRef = useRef<Points>(null!);
+
+  const { positions, colors } = useMemo(() => {
+    const positions = new Float32Array(maxPoints * 3);
+    const colors = new Float32Array(maxPoints * 3);
+
+    return { positions, colors };
+  }, [maxPoints]);
+
+  useEffect(() => {
+    const color = new Color();
+
+    for (let i = 0; i < points.length; i++) {
+      const [x, y, z] = points[i];
+
+      positions[i * 3 + 0] = x;
+      positions[i * 3 + 1] = y;
+      positions[i * 3 + 2] = z;
+
+      const alpha = (z - minZ) / (maxZ - minZ);
+
+      color.lerpColors(gradientFrom, gradientTo, alpha);
+
+      colors[i * 3 + 0] = color.r;
+      colors[i * 3 + 1] = color.g;
+      colors[i * 3 + 2] = color.b;
+    }
+
+    pointsRef.current.geometry.attributes.position.needsUpdate = true;
+    pointsRef.current.geometry.attributes.color.needsUpdate = true;
+    pointsRef.current.geometry.computeBoundingBox();
+    pointsRef.current.geometry.computeBoundingSphere();
+  }, [pointsRef, points, positions, colors]);
+
+  return (
+    <points ref={pointsRef}>
+      <bufferGeometry>
+        <bufferAttribute
+          attach="attributes-position"
+          count={points.length}
+          array={positions}
+          itemSize={3}
+          usage={DynamicDrawUsage}
+        />
+        <bufferAttribute
+          attach="attributes-color"
+          count={points.length}
+          array={colors}
+          itemSize={3}
+          usage={DynamicDrawUsage}
+        />
+      </bufferGeometry>
+      <pointsMaterial size={pointSize} vertexColors />
+    </points>
+  );
+}
