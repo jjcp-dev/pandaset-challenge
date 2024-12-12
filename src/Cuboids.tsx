281704895a091e84006e5c3691baf422681f0144
Juan Cabrera <juan@jjcp.dev>
2024-12-12 10:59:57 -0300

    Add raycasting support to the Cuboids component

--- src/Cuboids.tsx
+++ src/Cuboids.tsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Edges } from "@react-three/drei";
+import { CycleRaycast } from "@react-three/drei";
 
-import { Cuboid } from "./Cuboid";
 import { CuboidMesh } from "./CuboidMesh";
+import { Cuboid } from "./types/Cuboid";
+import { ThreeEvent } from "@react-three/fiber";
 
-export function Cuboids({ cuboids }: { cuboids: Cuboid[] }) {
+export type CuboidsProps = {
+  cuboids: Cuboid[];
+  onHover: (cuboids: Cuboid[]) => void;
+};
+
+export function Cuboids({ cuboids, onHover }: CuboidsProps) {
+  const [hovered, setHovered] = useState({});
+
   return (
     <>
       {cuboids.map((cuboid) => (
-        <CuboidMesh key={cuboid.uuid} cuboid={cuboid} />
+        <CuboidMesh
+          key={cuboid.uuid}
+          cuboid={cuboid}
+          hovered={cuboid.uuid in hovered}
+          onPointerOver={(e: ThreeEvent<MouseEvent>) => {
+            e.stopPropagation();
+          }}
+        />
       ))}
+      <CycleRaycast
+        onChanged={(objects, cycle) => {
+          const cuboids = objects.map((x) => x.object.userData.cuboid);
+
+          setHovered(
+            cuboids.reduce((acc, x) => {
+              acc[x.uuid] = x;
+              return acc;
+            }, {})
+          );
+
+          onHover(objects.map((x) => x.object.userData.cuboid));
+
+          return null;
+        }}
+      />
     </>
   );
 }
